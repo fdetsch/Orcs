c542a815366712afa5b98dd33964c2c3fa4ee1e3
Florian Detsch <fdetsch@pc19ui245>
2015-10-13 09:56:34 +0200

    Initial upload.

---
+++ R/Orcs-package.R
@@ -0,0 +1,18 @@
+#' Omnidirectional R Code Snippets
+#'
+#' The package provides a variety of functions which I regularly use during my 
+#' everyday work.
+#'
+#' @name Orcs-package
+#' @aliases orcspackage
+#' @docType package
+#' @title Omnidirectional R Code Snippets.
+#' @author Florian Detsch \cr
+#' \cr
+#' \emph{Maintainer:} Florian Detsch \email{florian.detsch@@staff.uni-marburg.de}
+#'
+#' @import methods raster
+#' 
+#' @keywords package
+#'
+NULL
